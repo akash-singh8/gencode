c6ce9b33cddc460b00a379d06b8ecb75ce1ba31c
Akash Singh <developer.akash8@gmail.com>
2024-07-12 22:51:32 +0530

    script: added to execute c and c++ codes

---
+++ scripts/execute_gcc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if docker ps -a --filter "name=lenx-cpp" --format '{{.Names}}' | grep -q "lenx-cpp"; then
+    echo "Container with the name lenx-cpp exists. Removing it..."
+    docker rm -f lenx-cpp
+fi
+
+FILE_NAME=$1
+
+docker run -d --name lenx-cpp gcc sleep infinity
+
+if [ $? -ne 0 ]; then
+    echo "Failed to start the container"
+    exit 1
+fi
+
+docker cp "scripts/${FILE_NAME}" lenx-cpp:"/home/${FILE_NAME}"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to copy the file to the container"
+    docker rm -f lenx-cpp
+    exit 1
+fi
+
+docker exec lenx-cpp g++ "home/${FILE_NAME}" -o home/cpp_exe
+
+if [ $? -ne 0 ]; then
+    echo "Failed to compile the ${FILE_NAME} file"
+    docker rm -f lenx-cpp
+    exit 1
+fi
+
+docker exec lenx-cpp home/cpp_exe
+
+if [ $? -ne 0 ]; then
+    echo "Failed to execute the compiled binary"
+    docker rm -f lenx-cpp
+    exit 1
+fi
+
+docker rm -f lenx-cpp

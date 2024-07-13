f245049f768d4ba3f98e01d5439df3f501e1e71e
Akash Singh <developer.akash8@gmail.com>
2024-07-13 19:38:05 +0530

    script: added to execute py codes

---
+++ scripts/execute_py.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if docker ps -a --filter "name=lenx-py" --format '{{.Names}}' | grep -q "lenx-py"; then
+    echo "Container with the name lenx-py exists. Removing it..."
+    docker rm -f lenx-py
+fi
+
+FILE_NAME=$1
+
+docker run -d --name lenx-py python sleep infinity
+
+if [ $? -ne 0 ]; then
+    echo "Failed to start the container"
+    exit 1
+fi
+
+docker cp "scripts/${FILE_NAME}" lenx-py:"/home/${FILE_NAME}"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to copy the file to the container"
+    docker rm -f lenx-py
+    exit 1
+fi
+
+docker exec lenx-py python "home/${FILE_NAME}"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to execute ${FILE_NAME} file"
+    docker rm -f lenx-py
+    exit 1
+fi
+
+docker rm -f lenx-py

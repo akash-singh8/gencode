d1d678a3e1a8094c5e955d412ec2fba763021e5a
Akash Singh <developer.akash8@gmail.com>
2024-08-06 22:57:15 +0530

    component: added terminal section

---
+++ src/components/Terminal.tsx
@@ -0,0 +1,7 @@
+import styles from "@/styles/terminal.module.scss";
+
+const Terminal = () => {
+  return <div className={styles.terminal}>terminal &nbsp; &nbsp; output</div>;
+};
+
+export default Terminal;
